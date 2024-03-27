ce538997927f03b81ca9ffed9e5ad53c0755c6d2
Is Teamoor <is.teamoor@gmail.com>
2024-03-27 14:15:11 +0500

    install vue

--- engine/components/block.js
+++ engine/components/block.js
@@ -2,7 +2,12 @@ export const block = {
   name: "my-block",
   template: `
     <div class='my-block'>
-        my block!
+        my block!{{platform}}
     </div>
     `,
+  data() {
+    return {
+      platform: window.Telegram.WebApp.platform,
+    };
+  },
 };
