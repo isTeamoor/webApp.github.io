d8a411356eaf49c362595071006139c215f0328d
Is Teamoor <is.teamoor@gmail.com>
2024-03-27 10:55:16 +0500

    install vue

--- engine/components/App.js
+++ engine/components/App.js
@@ -2,6 +2,10 @@ export const App = {
   name: "App",
 
   template: `
-  <div class='mainWindow'></div>
+  <div class='mainWindow'>
+    <h1>Greetings!</div>
+    <h2>Hi</div>
+    <button>Button</button>
+  </div>
   `,
 };
