9d7e16459c7f26b5c57d99408a07e1865620b50a
Is Teamoor <is.teamoor@gmail.com>
2024-04-08 15:32:30 +0500

    bids table

--- src/components/screens/UserProfile.vue
+++ src/components/screens/UserProfile.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        08.04.2024
         <p>User ID {{ userData.id }}</p>
         <p>First_name {{ userData.first_name }}</p>
         <p>Last_name {{ userData.last_name }}</p>
