baf32a5ed0b459865a28e65dc08bce8d7ce75879
Is Teamoor <is.teamoor@gmail.com>
2024-04-08 15:50:14 +0500

    bids table

--- src/components/screens/UserProfile.vue
+++ src/components/screens/UserProfile.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        08.04.2024
+        08.04.2024 - 15-49
+        __{{ this.tg }}__
         <p>User ID {{ userData.id }}</p>
         <p>First_name {{ userData.first_name }}</p>
         <p>Last_name {{ userData.last_name }}</p>
@@ -14,6 +15,7 @@ export default {
     data() {
         return {
             userData: {},
+            tg: Object.keys(window.Telegram)
         };
     },
     methods: {
